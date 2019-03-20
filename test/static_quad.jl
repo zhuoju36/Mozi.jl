43f61e28095de445620c290cc4744466aa5ecf5b
HZJ <zhuoju36@hotmail.com>
2019-03-21 01:17:20 +0800

    - bug fixed

--- test/static_quad.jl
+++ test/static_quad.jl
@@ -37,13 +37,13 @@ set_nodal_restraint!(st,2,true,true,true,true,true,true)
 lcset=LoadCaseSet()
 add_static_case!(lcset,"MEMB",0)
 add_static_case!(lcset,"BEND",0)
-add_nodal_force!(lcset,"MEMB",4,0,-1e8,0,0,0,0)
+add_nodal_force!(lcset,"MEMB",4,0,-1e5,0,0,0,0)
 add_nodal_force!(lcset,"BEND",4,0,0,-10,0,0,0)
 
 assembly=assemble!(st,lcset,path=PATH)
 solve(assembly)
 
-r=result_nodal_displacement(assembly,"BEND",3)
+r=result_nodal_displacement(assembly,"MEMB",4)
 @show r
 r=result_nodal_displacement(assembly,"BEND",4)
 @test r≈[0.0, 0.0, -1.06318, 0.799041, 0.226482, 0.0] atol=1e-3
@@ -60,17 +60,21 @@ add_quad!(st,1,4,3,2,1,"steel",1e-3)
 
 set_nodal_restraint!(st,1,true,true,true,true,true,true)
 set_nodal_restraint!(st,2,true,true,true,true,true,true)
+# for i in [3,4]
+#     set_nodal_restraint!(st,i,false,false,false,false,false,true)
+# end
+
 
 lcset=LoadCaseSet()
 add_static_case!(lcset,"MEMB",0)
 add_static_case!(lcset,"BEND",0)
-add_nodal_force!(lcset,"MEMB",4,0,-1e8,0,0,0,0)
+add_nodal_force!(lcset,"MEMB",4,0,-1e5,0,0,0,0)
 add_nodal_force!(lcset,"BEND",4,0,0,-10,0,0,0)
 
 assembly=assemble!(st,lcset,path=PATH)
 solve(assembly)
 
-r=result_nodal_displacement(assembly,"BEND",3)
+r=result_nodal_displacement(assembly,"MEMB",4)
 @show r
 r=result_nodal_displacement(assembly,"BEND",4)
 @test r≈[0.0, 0.0, -0.348873, 0.233849, 0.0296653, 0.0] atol=1e-3
